05a659392252a4ff4a8de698e409594f3adde49e
Elvis Guanoluisa <elviserock@gmail.com>
2023-06-24 23:51:11 -0500

    Enlistar portafolios

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,12 +1,12 @@
-const renderIndex = (req,res)=>{
-    res.render('index')
-}
+const Portfolio = require('../models/Portafolio')
 
+const renderIndex = async(req,res)=>{
+    const portfolios = await Portfolio.find().lean()
+    res.render('index',{portfolios})
+}
 const renderAbout = (req,res)=>{
     res.render('login')
 }
-
-
 module.exports ={
     renderIndex, 
     renderAbout
